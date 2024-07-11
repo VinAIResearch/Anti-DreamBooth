db2cb60dad5a8c17ff08b14a9d2d31f14fc9655c
thuangb <leejohnthuan@gmail.com>
2024-07-11 14:38:26 +0700

    project page

--- evaluations/deepface/scripts/service.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-cd ../api
-gunicorn --workers=1 --timeout=3600 --bind=0.0.0.0:5000 "app:create_app()"
--- evaluations/deepface/tests/stream.py
+++
@@ -1,8 +0,0 @@
-from deepface import DeepFace
-
-DeepFace.stream("dataset") #opencv
-#DeepFace.stream("dataset", detector_backend = 'opencv')
-#DeepFace.stream("dataset", detector_backend = 'ssd')
-#DeepFace.stream("dataset", detector_backend = 'mtcnn')
-#DeepFace.stream("dataset", detector_backend = 'dlib')
-#DeepFace.stream("dataset", detector_backend = 'retinaface')
